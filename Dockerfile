7bfa140889bf83dc50f4c98c5a97d848047b9f8c
Tuomas Palenius <tuomas.palenius@gmail.com>
2016-06-17 12:09:25 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+# this creates a base system for custom made IDE
+FROM ubuntu:latest
+MAINTAINER tuomas.palenius@gmail.com
+
+RUN apt-get update && apt-get install -y git nodejs npm ssh sudo
+
+# process manager:
+RUN npm install -g pm2
+
+# use node instead of nodejs to run node apps (nodejs installed from apt-get, conflicts with node package)
+RUN ln -s /usr/bin/nodejs /usr/bin/node
+
+# install ide
+RUN cd /opt && git clone https://github.com/artiee/zeroace.git
+
+# install wetty
+RUN cd /opt/zeroace && git clone https://github.com/krishnasrinivas/wetty && cd wetty && npm install
+
+# install file-browser
+RUN cd /opt/zeroace && git clone https://github.com/sumitchawla/file-browser.git && cd file-browser && npm install
+
+# create the start script:
+RUN echo "service ssh start && pm2 start /opt/zeroace/wetty/app.js -- -p 8087 --sshuser ide
+ --sshhost localhost --sshport 3022 && pm2 start /opt/zeroace/file-browser/index.js" > /opt/start.sh
+
+# create user ide with home dir and bash shell
+RUN useradd -d /home/ide -m -s /bin/bash ide
+
+# set password for ide (password: ide)
+RUN echo 'ide:ide' | chpasswd
+
+# give access to root
+RUN adduser ide sudo
+
+# grant ssh privilege
+RUN echo "AllowUsers ide" > /etc/ssh/sshd_config
+RUN echo "Port 3022" >> /etc/ssh/sshd_config
+
+# set environment variable
+ENV TERM=xterm
+
+# expose ports for wetty, file-browser, ssh (you still need to map them when starting container)
+EXPOSE 8087 8088 3022
+
+# start services and leave process running (so container will keep running)
+CMD sh /opt/start.sh && tail -F /var/log/faillog
+
---
+++ index.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var _ = require('lodash');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+
+var program = require('commander');
+
+function collect(val, memo) {
+  if(val && val.indexOf('.') != 0) val = "." + val;
+  memo.push(val);
+  return memo;
+}
+
+program
+  .option('-p, --port <port>', 'Port to run the file-browser. Default value is 8088')
+  .option('-e, --exclude <exclude>', 'File extensions to exclude. To exclude multiple extension pass -e multiple times. e.g. ( -e .js -e .cs -e .swp) ', collect, [])
+  .parse(process.argv);
+
+var app = express();
+var dir = "/opt"; // process.cwd();
+app.use(express.static(dir)); //app public directory
+app.use(express.static(__dirname)); //module directory
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+var server = http.createServer(app);
+
+if(!program.port) program.port = 8088;
+else console.log("Program port was already set: " + program.port);
+
+
+server.listen(program.port);
+console.log("Please open the link in your browser http://<YOUR-IP>:" + program.port);
+
+app.get('/files', function(req, res) {
+ var currentDir =  dir;
+ var query = req.query.path || '';
+ if (query) currentDir = path.join(dir, query);
+ console.log("browsing ", currentDir);
+ fs.readdir(currentDir, function (err, files) {
+     if (err) {
+        throw err;
+      }
+      var data = [];
+      files
+      .filter(function (file) {
+          return true;
+      }).forEach(function (file) {
+        try {
+                //console.log("processing ", file);
+                var isDirectory = fs.statSync(path.join(currentDir,file)).isDirectory();
+                if (isDirectory) {
+                  data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
+                } else {
+                  var ext = path.extname(file);
+                  if(program.exclude && _.contains(program.exclude, ext)) {
+                    console.log("excluding file ", file);
+                    return;
+                  }
+                  data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
+                }
+
+        } catch(e) {
+          console.log(e);
+        }
+
+      });
+      data = _.sortBy(data, function(f) { return f.Name });
+      res.json(data);
+  });
+});
+
+app.get('/', function(req, res) {
+  console.log("Received request to uri root");
+  res.redirect('wetty/public/lib/template.html'); 
+});
